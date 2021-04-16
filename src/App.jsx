4ec40e2130807b551b117d236d1703efbcce7bd2
Venkatesh Chakrabarty <venkatesh46.ch@gmail.com>
2021-04-17 00:44:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          This React App was created without using <div>npx create-react-app</div>!
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
